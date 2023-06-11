90a0ea9b0c7efcc3540366ee80f04d43310f1d0e
Ivan Trj <ivantrajanovski@gmail.com>
2023-06-11 10:42:28 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ivan Trajanovski on 11.06.23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
